c02445311c21380f7a97f641601d8f070eac643e
Alberto De Lucas <alberto5dlg@gmail.com>
2016-10-11 11:14:54 +0200

    Añadida clase para poner datos de inicio y que el programa no se encuentre vacio de avisos al comienzo

--- src/models/Utils.java
+++ src/models/Utils.java
@@ -1,5 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Utils {
+	
+	public static ArrayList<Fallo> rellenarAvisos()
+	{
+		ArrayList<Fallo> lista = new ArrayList<Fallo>();
+		String nombre; 
+		String descripcion; 
+		
+	}
 
 }
